b48723a21dd3350067f844ce772634d3fe89fa8a
Lin Tao <lintaoo@hotmail.com>
2017-08-09 18:20:29 -0400

    I updated binary search.

--- src/lt/BinarySearch.java
+++ src/lt/BinarySearch.java
@@ -1,5 +1,5 @@
 package lt;
-import java.util.*;
+
 /**
  * Created by Lin Tao on 6/28/2017.
  */
@@ -8,11 +8,9 @@ public class BinarySearch {
     // return -1
 
 
-    int binarySearch(int arr[], int l, int r, int x)
-    {
-        if (r>=l)
-        {
-            int mid = l + (r - l)/2;
+    int binarySearch(int arr[], int l, int r, int x) {
+        if (r >= l) {
+            int mid = l + (r - l) / 2;
 
             // If the element is present at the middle itself
             if (arr[mid] == x)
@@ -21,11 +19,11 @@ public class BinarySearch {
             // If element is smaller than mid, then it can only
             // be present in left subarray
             if (arr[mid] > x)
-                return binarySearch(arr, l, mid-1, x);
+                return binarySearch(arr, l, mid - 1, x);
 
             // Else the element can only be present in right
             // subarray
-            return binarySearch(arr, mid+1, r, x);
+            return binarySearch(arr, mid + 1, r, x);
         }
 
         // We reach here when element is not present in array
@@ -33,7 +31,6 @@ public class BinarySearch {
     }
 
     // Driver method to test above
-
 
 
 }
